e4c31534f04b26033e14a76151dbc724602eaf4c
Nullish <nullishusername@gmail.com>
2026-08-14 14:59:57 +0200

    Literally just a quick change to test blacksmith caching stuff!

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -14,7 +14,7 @@ export default function Home() {
     <div className="flex flex-col flex-1 items-center justify-center font-sans bg-zinc-50 dark:bg-gray-900">
       <h1> We are cooking up something awesome... </h1>
       <hr className="w-96 h-1 mx-auto my-4 border-0 rounded-sm md:my-10 bg-gray-900 dark:bg-amber-50" />
-      <p> Pardon the lack of detail, we will reach out when we have more info for you!</p>
+      <p> Pardon the lack of detail, we will reach out when we have more info for you. Check back soon!</p>
     </div>
   );
 }
